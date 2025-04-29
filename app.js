95bcadf844b5822cc83e04ad0d9da216ebfebaf4
Aman gwari <amangwari29sep@gmail.com>
2025-04-30 03:02:37 +0530

    commit change for // ==> Interfaces and Type Aliases // Defining interfaces // Using interfaces to define object shapes // Extending interfaces // Type aliases // Intersectin Types

--- app.js
+++ app.js
@@ -62,12 +62,73 @@ let adf3;
 // ==> undefined
 let sdf;
 // ==> Never
-function infiniteProcess() {
-    while (true) {
-        console.log("Processing...");
-    }
-    // This line will never be reached
-    // return "Done"; // Error: Function with 'never' return type cannot have a 'return' statement.
+// function infiniteProcess(): never {
+//     while (true) {
+//         console.log("Processing...");
+//     }
+//     // This line will never be reached
+//     // return "Done"; // Error: Function with 'never' return type cannot have a 'return' statement.
+// }
+// infiniteProcess(); // This function will run indefinitely, printing "Processing..."
+// console.log("hello") // unrechable code 
+// --------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------
+// => Type Inference
+let acc = 23;
+// => Type Annotations
+let amm;
+amm = 12;
+amm = false;
+amm = "sfsdf";
+// ==> Interfaces and Type Aliases
+// Defining interfaces
+// Using interfaces to define object shapes
+// Extending interfaces
+// Type aliases
+// Intersectin Types
+// Defining interfaces
+const user = {
+    name: "Aman",
+    email: "aman@gmail.com",
+    password: "ksddfoiwhewnv"
+};
+function abcde(obj) {
+    console.log(`${obj.email}_${obj.name}_${obj.password}`);
 }
-infiniteProcess(); // This function will run indefinitely, printing "Processing..."
-console.log("hello"); // unrechable code 
+abcde({ name: "aman", email: "amangwari@gmail.com", password: "sndfoen" });
+function foo(obj) {
+    // here only obj has properties of normaluser
+    console.log(`${obj.email}_${obj.name}_${obj.password}`);
+}
+foo({ name: "aman", email: "amangwari@gmail.com", password: "sndfoen" });
+function foo2(obj) {
+    // here only obj has properties of normaluser + its own i.e admin
+    console.log(`${obj.email}_${obj.name}_${obj.password}_${obj.isAdmin}`);
+}
+foo2({ name: "aman", email: "amangwari@gmail.com", password: "sndfoen", isAdmin: true });
+function newfoo(obj) {
+    // here i can access both email and name they got merged 
+    console.log(`${obj.name}_${obj.email}`);
+}
+newfoo({ name: "aman", email: "amangwari@gmail.com" });
+let asdfg;
+// might looking unsensiblle untill you see below example
+let add;
+let asadd;
+function newfooo(obj) {
+    console.log(`${obj}`);
+}
+newfooo("sdklfjsdfj");
+function abcddd(a) {
+    console.log(a.age);
+    console.log(a.name);
+    console.log(a.allowuser);
+    // a can aceess all properties of user & itself i.e adming
+}
+abcddd({ name: "aman", age: 2342, allowuser: true });
+// union Types (|)
+let valuee;
+valuee = "aman";
+valuee = 43;
+// valuee = false // throw error
--- app.ts
+++ app.ts
@@ -82,13 +82,144 @@ let sdf: undefined;
 
 
 // ==> Never
-function infiniteProcess(): never {
-    while (true) {
-        console.log("Processing...");
-    }
-    // This line will never be reached
-    // return "Done"; // Error: Function with 'never' return type cannot have a 'return' statement.
+// function infiniteProcess(): never {
+//     while (true) {
+//         console.log("Processing...");
+//     }
+//     // This line will never be reached
+//     // return "Done"; // Error: Function with 'never' return type cannot have a 'return' statement.
+// }
+
+// infiniteProcess(); // This function will run indefinitely, printing "Processing..."
+// console.log("hello") // unrechable code 
+
+// --------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------
+
+// => Type Inference
+let acc = 23;
+
+// => Type Annotations
+let amm: number | boolean | string;
+amm = 12;
+amm = false;
+amm = "sfsdf"
+
+
+// ==> Interfaces and Type Aliases
+// Defining interfaces
+// Using interfaces to define object shapes
+// Extending interfaces
+// Type aliases
+// Intersectin Types
+
+
+
+// Defining interfaces
+const user = {
+    name: "Aman",
+    email: "aman@gmail.com",
+    password: "ksddfoiwhewnv"
 }
 
-infiniteProcess(); // This function will run indefinitely, printing "Processing..."
-console.log("hello") // unrechable code 
+// converting to interface 
+interface user {
+    name: string,
+    email: string,
+    password: string,
+    gender?: string
+}
+
+function abcde(obj: user) {
+    console.log(`${obj.email}_${obj.name}_${obj.password}`)
+}
+
+abcde({ name: "aman", email: "amangwari@gmail.com", password: "sndfoen" })
+
+
+// => Extending Interfaces 
+interface normaluser {
+    name: string,
+    email: string,
+    password: string,
+}
+
+interface admin extends normaluser {
+    isAdmin: Boolean
+}
+
+function foo(obj: normaluser) {
+    // here only obj has properties of normaluser
+    console.log(`${obj.email}_${obj.name}_${obj.password}`)
+}
+
+foo({ name: "aman", email: "amangwari@gmail.com", password: "sndfoen" })
+
+function foo2(obj: admin) {
+    // here only obj has properties of normaluser + its own i.e admin
+    console.log(`${obj.email}_${obj.name}_${obj.password}_${obj.isAdmin}`)
+}
+
+foo2({ name: "aman", email: "amangwari@gmail.com", password: "sndfoen", isAdmin: true })
+
+// => Interface of same name gets merged
+
+interface amannew {
+    name: string
+}
+
+interface amannew {
+    email: string
+}
+
+function newfoo(obj: amannew) {
+    // here i can access both email and name they got merged 
+    console.log(`${obj.name}_${obj.email}`)
+}
+newfoo({ name: "aman", email: "amangwari@gmail.com" })
+
+
+// ==> Type aliases
+type sankhya = number;
+let asdfg: sankhya;
+// might looking unsensiblle untill you see below example
+
+let add: string | number | boolean;
+// insted of writing this you can write
+
+type newsankhya = string | number | boolean;
+let asadd: newsankhya
+
+function newfooo(obj: newsankhya) {
+    console.log(`${obj}`)
+}
+
+newfooo("sdklfjsdfj");
+
+
+
+// Intersectin Types (&)
+type user1 = {
+    name: string,
+    age: number
+}
+
+type adming = user1 & {
+    allowuser: true;
+}
+
+function abcddd(a: adming) {
+    console.log(a.age)
+    console.log(a.name)
+    console.log(a.allowuser)
+    // a can aceess all properties of user & itself i.e adming
+}
+
+abcddd({ name: "aman", age: 2342, allowuser: true })
+
+// union Types (|)
+let valuee: string | number
+valuee = "aman";
+valuee = 43;
+// valuee = false // throw error
